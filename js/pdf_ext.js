3c26d3851b16efafa5c61538f56b040fe2d4c73e
unknown <robbi.tugas@gmail.com>
2017-12-28 18:19:05 +0700

    js for pop up pdf

---
+++ js/pdf_ext.js
@@ -0,0 +1,11 @@
+$('a').on('click', function() {
+	var href = $(this).attr('href');
+	var rules = new RegExp('([a-zA-Z0-9\s_\\.\-:])+(.doc|.docx|.pdf)$');
+	if (rules.test(href)) {
+		var title = $(this).html();
+		$('#myModal').find('.modal-title').html(title);
+		$('#myModal').find('iframe').attr('src', href);
+		$('#myModal').modal('show');
+		return false;	
+	}
+});
