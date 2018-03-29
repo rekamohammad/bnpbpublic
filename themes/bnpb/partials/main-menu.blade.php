6631f9de4bd736ab97df237a49df2c633e02ce98
unknown <rama_dona14@yahoo.com>
2018-03-29 11:12:31 +0700

    bahasa

--- themes/bnpb/partials/main-menu.blade.php
+++ themes/bnpb/partials/main-menu.blade.php
@@ -2,7 +2,7 @@
     @foreach ($menu_nodes as $key => $row)
     <li class="menu-item @if ($row->hasChild()) menu-item-has-children dropdown @endif {{ $row->css_class }} @if ($row->getRelated(true)->url == Request::url()) active @endif">
         <a href="{{ $row->getRelated(true)->url }}" target="{{ $row->target }}">
-            @if ($row->icon_font)<i class='{{ trim($row->icon_font) }}'></i> @endif{{ $row->getRelated(true)->name }}
+            @if ($row->icon_font)<i class='{{ trim($row->icon_font) }}'></i> @endif{{ __($row->getRelated(true)->name) }}
         </a>
         @if ($row->hasChild())
             {!!
